1f5a55cfaeeacf040e154a310342706ce814dec3
CFE <hello@teamcfe.com>
2021-03-30 14:54:48 -0700

    3 - Install Node.js, Yarn, & Parcel.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const appEl = document.getElementById('app')
+
+const App = _ =>{
+    return <h1>Hello world</h1>
+}
+
+ReactDOM.render(<App />, appEl)
