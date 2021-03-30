97619a5c622d34a44ca15ecdce9aaabe862d611b
CFE <hello@teamcfe.com>
2021-03-30 15:16:56 -0700

    4 - Hello World

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const appEl = document.getElementById('cfe')
+
+const App = props => {
+    // jsx
+    return <h1>Hello World</h1>
+}
+
+ReactDOM.render(<App />, appEl)
+
