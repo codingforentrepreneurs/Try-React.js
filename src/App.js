391c74c967f84521f08b9b6432cf4d5cb7c46da7
CFE <hello@teamcfe.com>
2021-03-30 16:21:08 -0700

    9 - The useState Hooke

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
-
+    const [myCurrentCount, setCurrentCount] = useState(1)
     const handleClick = (event) =>{
         console.log(event)
-    }    
+        setCurrentCount(myCurrentCount + 1)
+        console.log(myCurrentCount)
+    }
     return <div>
         <h1>Hello World</h1>
-        <button onClick={handleClick}>Like</button>
+        <button onClick={handleClick}>Like {myCurrentCount}</button>
     </div>
 }
 
