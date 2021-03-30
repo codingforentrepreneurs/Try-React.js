a0639d3aa4fdbc988d1e6029ae2535a11cfd4934
CFE <hello@teamcfe.com>
2021-03-30 15:29:26 -0700

    6 - Importing and Exporting Components

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {App} from './App'
 
 const appEl = document.getElementById('cfe')
 
-const App = props => {
-    // jsx
-    return <h1>Hello World</h1>
+function Index () {
+    return <div>
+        <abc />
+    </div>
 }
-
 ReactDOM.render(<App />, appEl)
-
-
-const cfe2El = document.getElementById('cfe2')
-
-const CFE2 = props => {
-    // jsx
-    return <h1>CFE2</h1>
-}
-
-if (cfe2El) {
-    ReactDOM.render(<CFE2 />, cfe2El)
-}
