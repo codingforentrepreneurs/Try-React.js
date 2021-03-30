ba4bff5ceeefa55648db2e2685ccd6da31a375f7
CFE <hello@teamcfe.com>
2021-03-30 15:46:26 -0700

    7 - Different types of components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+class MyComp extends React.Component {
+
+    render () {
+        return <h1>Hello World</h1>
+    }
+}
+
+
+const Arrow = () => {
+
+    return <p>I'm an arrow</p>
+}
 
 export default function App () {
 
-    return <h1>Hello World</h1>
+    return <div>
+        <MyComp />
+        <Arrow />
+    </div>
 }
-
-// const App = () => {}
--- src/index.js
+++ src/index.js
@@ -4,10 +4,4 @@ import ReactDOM from 'react-dom';
 import {App} from './App'
 
 const appEl = document.getElementById('cfe')
-
-function Index () {
-    return <div>
-        <abc />
-    </div>
-}
 ReactDOM.render(<App />, appEl)
