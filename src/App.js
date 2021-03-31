8061577667578e1d017a57df1cd682675dc7e568
CFE <hello@teamcfe.com>
2021-03-31 13:49:07 -0700

    11 - Props for Default Like Count

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 import {LikeBtn} from './buttons/likeBtn'
 
+
 const App = () => {
-    return <div>
+    return <div id='my-app-div' className='text-red another-class'>
         <h1>Hello World</h1>
-        <LikeBtn />
+        <LikeBtn className='text-red' initCount={5001} />
+
+        <LikeBtn className='text-red' initCount={1} />
+
+        <LikeBtn className='text-red' initCount={51} />
     </div>
 }
 
--- src/buttons/likeBtn.js
+++ src/buttons/likeBtn.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 
-export const LikeBtn = () => {
-    const [count, setCount] = useState(1)
+export const LikeBtn = (props) => {
+    console.log('LikeBtn', props)
+    const [count, setCount] = useState(props.initCount ? props.initCount : 0)
     const handleClick = (event) =>{
         // send to my server
 
         // from server
         setCount(count + 1)
     }
-    return <button onClick={handleClick}>Like {count}</button>
+    return <button className={props.className} onClick={handleClick}>Like {count}</button>
 }
