764dd08d9db4e7a6a99f2574f0ed236926bbdf7f
CFE <hello@teamcfe.com>
2021-04-01 13:10:48 -0700

    17 - Custom Hooks Part 1

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React from 'react';
 
 
 import {LikeBtn} from './buttons/likeBtn'
-import {DarkModeProvider, useDarkMode} from './darkMode/context'
+import {useDarkMode} from './darkMode/context'
 
 import {DarkModeBtn} from './darkMode/button'
+import {useCountdown} from './hooks/useCountdown'
 
 const App = () => {
+    const currentTime = useCountdown(2)
     const darkModeContext = useDarkMode()
     const isDark = darkModeContext.isDark
     const wrapperClassName = isDark ? 'bg-dark text-light' : 'bg-light text-dark'
-    
+
     return <div className={wrapperClassName}>
         <h1>Hello World {isDark? 'dark': 'light'}</h1>
+        <p>Your time is {currentTime}</p>
         <LikeBtn className='text-red'>Like</LikeBtn>
         <DarkModeBtn />
     </div>
